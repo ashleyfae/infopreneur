bcf2d4aac8dbcced9078d970d9717444fb11493d
Ashley Evans <cazzyae@gmail.com>
2016-10-21 11:01:10 +0100

    Added live preview for footer text colour.

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -279,6 +279,13 @@
         });
     });
 
+    // Footer text colour
+    wp.customize('footer_color', function (value) {
+        value.bind(function (to) {
+            $('#colophon, #colophon a').color(to);
+        });
+    });
+
     // Copyright
     wp.customize('copyright_message', function (value) {
         value.bind(function (to) {
